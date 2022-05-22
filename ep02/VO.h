0579427791850d8a03a1ce990c3bb4d7e1643675
Gabriel Arrais <arrais.gbr@usp.br>
2022-05-22 19:00:24 -0300

    VO final

--- ep02/VO.h
+++ ep02/VO.h
@@ -25,7 +25,7 @@ class VO : public Tree<Key, Item>{
         void print();
     
     private:
-        int shift(Key key);
+        int shift(int first);
         void resize();
         int binarySearch(Key key);
 
@@ -35,28 +35,30 @@ template<typename Key, typename Item>
 void VO<Key, Item>::add(Key key, Item value){
     Node<Key, Item> newNode = Node<Key, Item>(key, value);
     if(this->size == this->maxSize) resize();
-    int pos = shift(key);
     if(this->size == 0){
         this->array[0] = newNode;
         this->size++;
         return;
     }
-    if(this->array[pos].key == key) this->array[pos].value = value;
-    else{
-        this->array[pos] = newNode;
-        this->size++;
+    int pos = binarySearch(key);
+    if(this->array[pos - 1].key == key){
+        this->array[pos - 1].value = value;
+        return;
     }
+    shift(pos);
+    this->array[pos] = newNode;
+    this->size++;
 }
 
 template<typename Key, typename Item>
 Item VO<Key, Item>::value(Key key){
-    int pos = binarySearch(key);
+    int pos = binarySearch(key) - 1;
     return this->array[pos].value;
 }
 
 template<typename Key, typename Item>
 int VO<Key, Item>::rank(Key key){
-    int pos = binarySearch(key);
+    int pos = binarySearch(key) - 1;
     return pos;
 }
 
@@ -70,12 +72,9 @@ void VO<Key, Item>::print(){
 }
 
 template<typename Key, typename Item>
-int VO<Key, Item>::shift(Key key){
-    int i = this->size - 1;
-    for( ; this->array[i].key > key && i >= 0 ; i--)
+int VO<Key, Item>::shift(int first){
+    for(int i = this->size ; i >= first ; i--)
         this->array[i+1] = this->array[i];
-    
-    return i+1;
 }
 
 template<typename Key, typename Item>
@@ -91,10 +90,15 @@ template<typename Key, typename Item>
 int VO<Key, Item>::binarySearch(Key key){
     int l = 0;
     int r = this->size;
-    while(l <= r){
-        int m = (r + l) / 2;
-        if(this->array[m].key == key) return m;
-        else if(this->array[m].key > key) r = m-1;
-        else l = m+1;
+    int pos;
+    while(l < r){
+        int m = (l + r) / 2;
+        if(this->array[m].key <= key){
+            l = m + 1;
+        }
+        else r = m;
     }
+
+    if(l < this->size && this->array[l].key <= key) l++;
+    return l;
 }
