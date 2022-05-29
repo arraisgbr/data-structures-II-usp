84073eb1bc5908be36727d9d84d3bcb33e6bc9b7
Gabriel Arrais <arrais.gbr@usp.br>
2022-05-28 23:47:28 -0300

    main final

--- ep02/Node.h
+++ ep02/Node.h
@@ -54,11 +54,3 @@ class NodeRB : public Node<Key, Item>{
             this->right = NULL;
         }
 };
-
-template<typename Key, typename Item>
-class Node23 : public Node<Key, Item>{
-    public:
-        bool full;
-        Node23 *pai;
-        Node23 *left, *mid, *right;
-};
--- ep02/main.cpp
+++ ep02/main.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
+#include<sstream>
 #include "HT.h"
 #include "VO.h"
 #include "ABB.h"
 #include "TR.h"
 #include "ARN.h"
-#include "A23.h"
 
 using namespace std;
 
@@ -18,7 +18,6 @@ int main(){
     else if(type == "ABB") ht = new ABB<string, int>();
     else if(type == "TR") ht = new TR<string, int>();
     else if(type == "ARN") ht = new ARN<string, int>();
-    else ht = new A23<string, int>();
 
     int numberWords; cin >> numberWords; // número de palavras
     string *words = new string[numberWords];
@@ -31,22 +30,26 @@ int main(){
     int numberOperations; cin >> numberOperations;
     for(int i = 0 ; i < numberOperations ; i++){
         int operation; string parameter;
+        stringstream toInt;
+        int num;
+        cin >> operation >> parameter;
         switch(operation){
             case 1:
-                parameter = stoi(parameter);
-                for(int i = 0 ; i < parameter ; i++){
+                toInt << parameter;
+                toInt >> num;
+                for(int i = 0 ; i < num ; i++){
                     ht->add(words[currentWord], 1);
                     currentWord++;
                 }
                 break;
             case 2:
-                cout << ht->value(parameter);
+                cout << ht->value(parameter) << endl;
                 break;
             case 3:
-                cout << ht->rank(parameter);
+                cout << ht->rank(parameter) << endl;
                 break;
             case 4:
-                cout << ht->select(parameter);
+                cout << ht->select(num) << endl;
                 break;
         }
     }
